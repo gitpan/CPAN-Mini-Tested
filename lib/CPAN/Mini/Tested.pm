bfcefae10a6c3e0e282c8d53566189cff142d454
Barbie <barbie@missbarbell.co.uk>
2014-08-28 02:04:28 +0000

    Import of BARBIE/CPAN-Mini-Tested-0.23 from CPAN.
    gitpan-cpan-distribution: CPAN-Mini-Tested
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         BARBIE/CPAN-Mini-Tested-0.23.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/CPAN/Mini/Tested.pm
+++ lib/CPAN/Mini/Tested.pm
@@ -1,204 +1,214 @@
 package CPAN::Mini::Tested;
-use base 'CPAN::Mini';
 
 use 5.006;
 use strict;
 use warnings;
 
-use Cache::Simple::TimedExpiry 0.22;
+our $VERSION = '0.23';
 
+#--------------------------------------------------------------------------
+
+###########################################################################
+# Inheritence
+
+use base 'CPAN::Mini';
+
+###########################################################################
+# Modules
+
+use Cache::Simple::TimedExpiry  0.22;
 use Config;
 use DBI;
-use DBD::SQLite 1.00;
+use DBD::SQLite                 1.00;
+use File::Basename              qw( basename );
+use File::Spec::Functions       qw( catfile );
+use LWP::Simple                 qw( mirror RC_OK RC_NOT_MODIFIED );
+use Regexp::Assemble            0.06;
 
-use File::Basename qw( basename );
-use File::Spec::Functions qw( catfile );
+###########################################################################
+# Variables
 
-use LWP::Simple qw(mirror RC_OK RC_NOT_MODIFIED);
-use Regexp::Assemble 0.06;
+my $TESTDB  = 'cpanstats.db';
+my $TESTURL = 'http://devel.cpantesters.org/cpanstats.db';
 
-our $VERSION = '0.22';
+#--------------------------------------------------------------------------
 
-sub _dbh {
-  my $self = shift;
-  return $self->{test_db};
-}
-
-sub _sth {
-  my $self = shift;
-  return $self->{test_db_sth};
-}
-
-sub _connect {
-  my ($self, $database)  = @_;
-
-  $database ||= $self->{test_db_file};
-
-  unless (-r $database) {
-    die "Cannot find database file";
-  }
-
-  $self->{test_db} = DBI->connect(
-    "DBI:SQLite:dbname=".$database, "", "", {
-      RaiseError => 1,
-      %{$self->{test_db_conn} || { }},
-    },
-  ) or die "Unable to connect: ", $DBI::errstr;
-
-  # TODO: support for additional reports fields such as perl version
-
-  $self->{test_db_sth} =
-    $self->_dbh->prepare( qq{
-      SELECT COUNT(id) FROM reports
-      WHERE status='PASS' AND distribution=? AND version=? AND osname=?
-  }) or die "Unable to create prepare statement: ", $self->_dbh->errstr;
-
-  return 1;
-}
-
-
-sub _disconnect {
-  my $self = shift;
-  if ($self->_dbh) {
-    $self->_sth->finish if ($self->_sth);
-    $self->_dbh->disconnect;
-  }
-  return 1;
-}
+###########################################################################
+# Public Interface
 
 sub file_allowed {
-  my ($self, $file) = @_;
-  return (basename($file) eq 'testers.db') ? 1 :
-    CPAN::Mini::file_allowed($self, $file);
+    my ($self, $file) = @_;
+    return (basename($file) eq $TESTDB) ? 1 :
+        CPAN::Mini::file_allowed($self, $file);
 }
 
 sub mirror_indices {
-  my $self = shift;
+    my $self = shift;
 
-  if (defined $self->{test_db_arch}) {
-    warn "test_db_arch is deprecated";
-  }
+    warn "test_db_arch is deprecated"   if(defined $self->{test_db_arch});
 
-  $self->{test_db_file} ||= catfile($self->{local}, 'testers.db');
-  my $local_file = $self->{test_db_file};
+    $self->{test_db_file} ||= catfile($self->{local}, $TESTDB);
+    my $local_file = $self->{test_db_file};
 
-  # test_db_age < 0, do not update it
+    # test_db_age < 0, do not update it
 
-  my $test_db_age = $self->{test_db_age};
-     $test_db_age = 1, unless (defined $test_db_age);
+    my $test_db_age = $self->{test_db_age};
+    $test_db_age = 1, unless (defined $test_db_age);
 
-  if ( ($self->{force}) || (!-e $local_file) ||
-       (($test_db_age >= 0) &&
-	(-e $local_file) && ((-M $local_file) > $test_db_age)) ){
-    $self->trace('testers.db');
-    my $db_src = $self->{test_db_src} ||
-      'http://testers.cpan.org/testers.db';
-    my $status = mirror($db_src, $local_file);
+    if (    $self->{force}
+         || !-e $local_file
+         || (   $test_db_age >= 0 
+             && -e $local_file 
+             && -M $local_file > $test_db_age) ) {
 
-    if ($status == RC_OK) {
-      $self->trace(" ... updated\n");
-    } elsif ($status == RC_NOT_MODIFIED) {
-      $self->trace(" ... up to date\n");
-    } else {
-      warn "\n$db_src: $status\n";
-      return;
+        $self->trace($TESTDB);
+        my $db_src = $self->{test_db_src} || $TESTURL;
+        my $status = mirror($db_src, $local_file);
+
+        if ($status == RC_OK) {
+            $self->trace(" ... updated\n");
+        } elsif ($status == RC_NOT_MODIFIED) {
+            $self->trace(" ... up to date\n");
+        } else {
+            warn "\n$db_src: $status\n";
+            return;
+        }
     }
 
-  }
-  $self->_connect() if (-r $local_file);
+    $self->_connect() if (-r $local_file);
 
-  return CPAN::Mini::mirror_indices($self);
+    return CPAN::Mini::mirror_indices($self);
 }
 
 sub clean_unmirrored {
-  my $self = shift;
-  $self->_disconnect();
-  return CPAN::Mini::clean_unmirrored($self);
+    my $self = shift;
+    $self->_disconnect();
+    return CPAN::Mini::clean_unmirrored($self);
+}
+
+###########################################################################
+# Private Methods
+
+sub _dbh {
+    my $self = shift;
+    return $self->{test_db};
+}
+
+sub _sth {
+    my $self = shift;
+    return $self->{test_db_sth};
+}
+
+sub _connect {
+    my ($self, $database)  = @_;
+
+    $database ||= $self->{test_db_file};
+    die "Cannot find database file" unless (-r $database);
+
+    $self->{test_db} = DBI->connect(
+        "DBI:SQLite:dbname=".$database, "", "", {
+            RaiseError => 1,
+            %{$self->{test_db_conn} || { }},
+        },
+    ) or die "Unable to connect: ", $DBI::errstr;
+
+    # TODO: support for additional reports fields such as perl version
+
+    $self->{test_db_sth} =
+        $self->_dbh->prepare( qq{
+            SELECT COUNT(id) 
+              FROM cpanstats
+             WHERE status='PASS' 
+               AND dist=? 
+               AND version=? 
+               AND osname=?
+    }) or die "Unable to create prepare statement: ", $self->_dbh->errstr;
+
+    return 1;
+}
+
+sub _disconnect {
+    my $self = shift;
+    if ($self->_dbh) {
+        $self->_sth->finish if ($self->_sth);
+        $self->_dbh->disconnect;
+    }
+    return 1;
 }
 
 sub _check_db {
-  my ($self, $dist, $ver, $osname) = @_;
+    my ($self, $dist, $ver, $osname) = @_;
 
-  my $sth = $self->_sth;
-  unless ($sth) {
-    die "Not connected to the database\n";
-  }
+    my $sth = $self->_sth;
+    die "Not connected to the database\n"   unless ($sth);
 
-  $sth->execute($dist, $ver, $osname);
-  my $row = $sth->fetch;
+    $sth->execute($dist, $ver, $osname);
+    my $row = $sth->fetch;
 
-  if ($row) { return $row->[0]; } else { return 0; }
+    return $row->[0]    if($row);
+    return 0;
 }
 
 sub _reset_cache {
-  my $self = shift;
-  $self->{test_db_cache} = undef, if ($self->{test_db_cache});
-  $self->{test_db_cache} = new Cache::Simple::TimedExpiry;
-  $self->{test_db_cache}->expire_after($self->{test_db_cache_expiry} || 300);
+    my $self = shift;
+    $self->{test_db_cache} = undef, if ($self->{test_db_cache});
+    $self->{test_db_cache} = Cache::Simple::TimedExpiry->new;
+    $self->{test_db_cache}->expire_after($self->{test_db_cache_expiry} || 300);
 }
 
 sub _passed {
-  my ($self, $path) = @_;
+    my ($self, $path) = @_;
 
-  # CPAN::Mini 0.36 no longer calls the filter routine multiple times
-  # per module, but it will for packages with multiple modules. So we
-  # cache the results, but only for a limited time.
+    # CPAN::Mini 0.36 no longer calls the filter routine multiple times
+    # per module, but it will for packages with multiple modules. So we
+    # cache the results, but only for a limited time.
 
-  unless (defined $self->{test_db_cache}) {
-    $self->_reset_cache;
-  }
+    $self->_reset_cache unless (defined $self->{test_db_cache});
 
-  if ($self->{test_db_exceptions}) {
+    if ($self->{test_db_exceptions}) {
 
-    if (ref($self->{test_db_exceptions}) eq "CODE") {
-      return 1, if ( &{ $self->{test_db_exceptions} }($path) );
+        if (ref($self->{test_db_exceptions}) eq "CODE") {
+            return 1, if ( &{ $self->{test_db_exceptions} }($path) );
+        } else {
+            my $re = new Regexp::Assemble;
+
+            if (ref($self->{test_db_exceptions}) eq "ARRAY") {
+	            $re->add( @{ $self->{test_db_exceptions} } );
+            } elsif ( (!ref($self->{test_db_exceptions})) || (ref($self->{test_db_exceptions}) eq "Regexp") ) {
+	            $re->add( $self->{test_db_exceptions} );
+            } else {
+	            die "Unknown test_db_exception type: ", ref($self->{test_db_exceptions});
+            }
+
+            return 1, if ($path =~ $re->re);
+        }
     }
-    else {
 
-      my $re = new Regexp::Assemble;
+    return $self->{test_db_cache}->fetch($path) if ($self->{test_db_cache}->has_key($path));
 
-      if (ref($self->{test_db_exceptions}) eq "ARRAY") {
-	$re->add( @{ $self->{test_db_exceptions} } );
-      } elsif ( (!ref($self->{test_db_exceptions})) || 
-		(ref($self->{test_db_exceptions}) eq "Regexp") ) {
-	$re->add( $self->{test_db_exceptions} );
-      } else {
-	die "Unknown test_db_exception type: ",
-	  ref($self->{test_db_exceptions});
-      }
+    my $count = 0;
 
-      return 1, if ($path =~ $re->re);
+    my $distver = basename($path);
+    $distver =~ s/\.(tar\.gz|tar\.bz2|zip)$//;
+
+    my $x       = rindex($distver, '-');
+    my $dist    = substr($distver, 0, $x);
+    my $ver     = substr($distver, $x+1);
+
+    $self->{test_db_os} ||= $Config{osname};
+
+    if (ref($self->{test_db_os}) eq 'ARRAY') {
+        my @archs = @{ $self->{test_db_os} };
+        while ( (!$count) && (my $arch = shift @archs) ) {
+            $count += $self->_check_db($dist, $ver, $arch);
+        }
+    } else {
+        $count += $self->_check_db($dist, $ver, $self->{test_db_os});
     }
-  }
 
-  if ($self->{test_db_cache}->has_key($path)) {
-    return $self->{test_db_cache}->fetch($path);
-  }
+    $self->{test_db_cache}->set($path, $count);
 
-  my $count = 0;
-
-  my $distver = basename($path);
-  $distver =~ s/\.(tar\.gz|tar\.bz2|zip)$//;
-
-  my $x       = rindex($distver, '-');
-  my $dist    = substr($distver, 0, $x);
-  my $ver     = substr($distver, $x+1);
-
-  $self->{test_db_os} ||= $Config{osname};
-
-  if (ref($self->{test_db_os}) eq 'ARRAY') {
-    my @archs = @{ $self->{test_db_os} };
-    while ( (!$count) && (my $arch = shift @archs) ) {
-      $count += $self->_check_db($dist, $ver, $arch);
-    }
-  }
-  else {
-    $count += $self->_check_db($dist, $ver, $self->{test_db_os});
-  }
-
-  $self->{test_db_cache}->set($path, $count);
-
-  return $count;
+    return $count;
 }
 
 # TODO: if filtering in CPAN::Mini is changed to allow paths to be
@@ -206,35 +216,35 @@ sub _passed {
 # version which passes tests.
 
 sub _filter_module {
-  my ($self, $args) = @_;
-  return CPAN::Mini::_filter_module($self, $args)
-    || (!$self->_passed($args->{path}));
+    my ($self, $args) = @_;
+    return CPAN::Mini::_filter_module($self, $args)
+        || (!$self->_passed($args->{path}));
 }
 
 1;
-__END__
 
+__END__
 
 =head1 NAME
 
-CPAN::Mini::Tested - create a CPAN mirror using modules that have passed tests
+CPAN::Mini::Tested - Create a CPAN mirror using modules with passing test reports
 
 =head1 SYNOPSYS
 
   use CPAN::Mini::Tested;
 
   CPAN::Mini::Tested->update_mirror(
-   remote => "http://cpan.mirrors.comintern.su",
-   local  => "/usr/share/mirrors/cpan",
-   trace  => 1
+      remote => "http://cpan.mirrors.comintern.su",
+      local  => "/usr/share/mirrors/cpan",
+      trace  => 1
   );
 
 =head1 DESCRIPTION
 
-This module is a subclass of L<CPAN::Mini> which checks the CPAN
+This module is a subclass of L<CPAN::Mini> which checks a CPAN
 Testers database for passing tests of that distribution on your
-platform.  Distributions will only be downloaded if there are passing
-tests.
+platform. Distributions will only be downloaded if there have passing
+test reports.
 
 The major differences are that it will download the F<testers.db> file
 from the CPAN Testers web site when updating indices, and it will
@@ -248,7 +258,7 @@ The following additional options are supported:
 =item test_db_exceptions
 
 A Regexp or array of Regexps (or Regexp strings) of module paths that
-will be included in the mirror even if there are no passed tests for
+will be included in the mirror even if there are no PASS reports for
 them.
 
 If it is a code reference, then it refers to a subroutine which takes
@@ -271,12 +281,13 @@ Note that the testers database can be quite large (over 15MB).
 
 =item test_db_src
 
-When to download the latest copy of the testers database. Defaults to
-L<http://testers.cpan.org/testers.db>.
+Where to download the latest copy of the CPAN Testers database. Defaults to
+L<http://devel.cpantesters.org/cpanstats.db>, however please note this file 
+is no longer updated since April 2013.
 
 =item test_db_file
 
-The location of the local copy of the testers database. Defaults to
+The location of the local copy of the CPAN Testers database. Defaults to
 the root directory of C<local>.
 
 =item test_db_os
@@ -318,12 +329,12 @@ notes if it has not passed any tests.
 
 =item mirror_indices
 
-Downloads the latest F<testers.db> database file (if needed) and
+Downloads the latest F<cpanstats.db> database file (if needed) and
 connects to the database before it begins downloading indices.
 
 =item file_allowed
 
-Also notes if the file is the F<testers.db> databse file.
+Also notes if the file is the F<cpanstats.db> databse file.
 
 =item clean_unmirrored
 
@@ -346,35 +357,26 @@ If the way filters are handled in CPAN::Mini is changed in the future,
 then some of these issues can be resolved by downloading the most
 recent version which has passed tests.
 
-=head1 DSLIP
-
-  b - Beta testing
-  d - Developer
-  p - Perl-only
-  O - Object oriented
-  p - Standard Perl
-
-See L<http://cpan.uwinnipeg.ca/htdocs/faqs/dslip.html>
-
-=head1 AUTHOR
-
-Robert Rothenberg <rrwo at cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2005 by Robert Rothenberg.  All Rights Reserved.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.3 or,
-at your option, any later version of Perl 5 you may have available.
-
 =head1 SEE ALSO
 
 L<CPAN::Mini>
 
 L<CPAN::WWW::Testers>
 
-CPAN Testers L<http://testers.cpan.org>
+CPAN Testers L<http://cpantesters.org>
+
+=head1 AUTHOR
+
+Robert Rothenberg <rrwo at cpan.org>
+
+Barbie <barbie@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005-2014 by Robert Rothenberg.  All Rights Reserved.
+Copyright (C) 2014      by Barbie.
+
+This distribution is free software; you can redistribute it and/or
+modify it under the Artistic Licence v2.
 
 =cut
-
